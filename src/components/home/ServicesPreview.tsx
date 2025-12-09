4e82b489c3058222e443d077ae10af91bfcfa3cb
jatinlifo <jatinstudentjatin@gmail.com>
2025-12-09 22:46:41 +0530

    new change

--- src/components/home/ServicesPreview.tsx
+++ src/components/home/ServicesPreview.tsx
@@ -77,7 +77,7 @@ const ServicesPreview = () => {
               </div>
               
               <div className="absolute bottom-0 left-0 right-0 p-6 text-primary-foreground">
-                <h3 className="font-display text-2xl md:text-2xl font-bold  md:mb-4">{product.title}</h3>
+                <h3 className="font-display text-xl md:text-2xl font-bold  md:mb-4">{product.title}</h3>
                 <div className="grid grid-cols-2 gap-2  md:mb-4">
                   {product.features.map((feature) => (
                     <div key={feature} className="flex items-center gap-2 text-sm">
